df2163c89c56aa24320591ddf820e68d37886571
nadpescador <santiago.calvi@davinci.edu.ar>
2020-07-25 21:19:57 -0700

    Nueva branch con cambios en interfaz

--- src/Formularios/Juego.java
+++ src/Formularios/Juego.java
@@ -20,6 +20,14 @@ public class Juego extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel3 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        Neptuno = new javax.swing.JLabel();
+        EstrellaMuerte = new javax.swing.JLabel();
+        AgujeroNegro = new javax.swing.JLabel();
+        PlanetaRoto = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        Cucurucho = new javax.swing.JLabel();
         Dado = new javax.swing.JButton();
         Marte = new javax.swing.JLabel();
         Venus = new javax.swing.JLabel();
@@ -27,30 +35,61 @@ public class Juego extends javax.swing.JFrame {
         Jupiter = new javax.swing.JLabel();
         Volver = new javax.swing.JButton();
         Universo = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        Dado.setBackground(new java.awt.Color(255, 153, 0));
+        jLabel3.setText("Hola Willy te comere el peluquin");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 170, 110, 80));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/dialogo.png"))); // NOI18N
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 130, -1, -1));
+
+        Neptuno.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/neptuno.png"))); // NOI18N
+        getContentPane().add(Neptuno, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 20, -1, -1));
+
+        EstrellaMuerte.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/estrelladelamuerte.png"))); // NOI18N
+        getContentPane().add(EstrellaMuerte, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 270, -1, -1));
+
+        AgujeroNegro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/agujeronegro.png"))); // NOI18N
+        getContentPane().add(AgujeroNegro, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 230, 150, -1));
+
+        PlanetaRoto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/planetaroto.png"))); // NOI18N
+        getContentPane().add(PlanetaRoto, new org.netbeans.lib.awtextra.AbsoluteConstraints(910, 20, -1, -1));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Astronauta.png"))); // NOI18N
+        jLabel1.setText("jLabel1");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 0, -1, -1));
+
+        Cucurucho.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/cucurucho.png"))); // NOI18N
+        getContentPane().add(Cucurucho, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, -30, -1, -1));
+
+        Dado.setBackground(new java.awt.Color(255, 255, 255));
         Dado.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/dado.png"))); // NOI18N
         Dado.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 DadoMouseClicked(evt);
             }
         });
-        getContentPane().add(Dado, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 660, -1, -1));
+        Dado.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DadoActionPerformed(evt);
+            }
+        });
+        getContentPane().add(Dado, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 640, -1, -1));
 
         Marte.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/marte_solo.png"))); // NOI18N
-        getContentPane().add(Marte, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 420, -1, -1));
+        getContentPane().add(Marte, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 570, -1, -1));
 
-        Venus.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/venus_solo.png"))); // NOI18N
-        getContentPane().add(Venus, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 330, -1, -1));
+        Venus.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/venus.png"))); // NOI18N
+        getContentPane().add(Venus, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 420, 150, -1));
 
         Tierra.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/tierra_p1_p2.png"))); // NOI18N
-        getContentPane().add(Tierra, new org.netbeans.lib.awtextra.AbsoluteConstraints(990, 350, -1, -1));
+        getContentPane().add(Tierra, new org.netbeans.lib.awtextra.AbsoluteConstraints(1010, 420, -1, -1));
 
         Jupiter.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Jupiter_vacio.png"))); // NOI18N
-        getContentPane().add(Jupiter, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 400, 160, 200));
+        getContentPane().add(Jupiter, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 540, 160, 200));
 
         Volver.setFont(new java.awt.Font("Roboto Thin", 0, 18)); // NOI18N
         Volver.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Boton volver.png"))); // NOI18N
@@ -60,11 +99,15 @@ public class Juego extends javax.swing.JFrame {
                 VolverActionPerformed(evt);
             }
         });
-        getContentPane().add(Volver, new org.netbeans.lib.awtextra.AbsoluteConstraints(1070, 700, 170, 50));
+        getContentPane().add(Volver, new org.netbeans.lib.awtextra.AbsoluteConstraints(1040, 690, 180, 50));
 
+        Universo.setBackground(new java.awt.Color(255, 255, 255));
         Universo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Space.png"))); // NOI18N
         Universo.setText("jLabel1");
         getContentPane().add(Universo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1281, 777));
+
+        jLabel5.setText("jLabel5");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 280, -1, -1));
 
         pack();
         setLocationRelativeTo(null);
@@ -82,6 +125,10 @@ public class Juego extends javax.swing.JFrame {
         int dado = rnd.nextInt(3);
         int mensajeAleatorio = rnd.nextInt(2) + 1;
         System.out.println(dado);
+        
+        for (int i = 0; i < dado; i++) {
+            jLabel3.setText(mensajesMovimiento[i]);
+        }
 
         // Turno de Jugador
         if (azul > naranja) {
@@ -115,6 +162,7 @@ public class Juego extends javax.swing.JFrame {
             }
         }
 
+        
         if (posicionNaranja == posicionAzul) {
             switch (posicionAzul) {
                 case 1:
@@ -122,19 +170,44 @@ public class Juego extends javax.swing.JFrame {
                     Jupiter.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Jupiter_vacio.png")));
                     Venus.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/venus_solo.png")));
                     Tierra.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/tierra_vacio.png")));
+                    Cucurucho.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/cucurucho.png")));
+                    Neptuno.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/neptuno.png")));
+                    
                     break;
                 case 2:
                     Jupiter.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenesPlanetas/jupiter_2_astronautas.png")));
                     Venus.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/venus_solo.png")));
                     Tierra.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/tierra_vacio.png")));
                     Marte.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/marte_solo.png")));
+                    Cucurucho.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/cucurucho.png")));
+                    Neptuno.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/neptuno.png")));
                     break;
                 case 3:
                     Venus.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenesPlanetas/venus_2_astronautas.png")));
                     Marte.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/marte_solo.png")));
                     Jupiter.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Jupiter_vacio.png")));
                     Tierra.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/tierra_vacio.png")));
+                    Cucurucho.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/cucurucho.png")));
+                    Neptuno.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/neptuno.png")));
                     break;
+                case 4:
+                    break;
+                case 5:
+                    Venus.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenesPlanetas/venus_solo.png")));
+                    Marte.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/marte_solo.png")));
+                    Jupiter.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Jupiter_vacio.png")));
+                    Tierra.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/tierra_vacio.png")));
+                    Cucurucho.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/cucurucho.png")));
+                    Neptuno.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/neptuno_p1_p2.png")));
+                break;
+                case 6:
+                    Venus.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenesPlanetas/venus_solo.png")));
+                    Marte.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/marte_solo.png")));
+                    Jupiter.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Jupiter_vacio.png")));
+                    Tierra.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/tierra_vacio.png")));
+                    Cucurucho.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/cucurucho_p1_p2.png")));
+                    Neptuno.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/neptuno.png")));
+                        
 
             }
 
@@ -231,6 +304,10 @@ public class Juego extends javax.swing.JFrame {
 
     }//GEN-LAST:event_DadoMouseClicked
 
+    private void DadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DadoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_DadoActionPerformed
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -264,12 +341,21 @@ public class Juego extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel AgujeroNegro;
+    private javax.swing.JLabel Cucurucho;
     private javax.swing.JButton Dado;
+    private javax.swing.JLabel EstrellaMuerte;
     private javax.swing.JLabel Jupiter;
     private javax.swing.JLabel Marte;
+    private javax.swing.JLabel Neptuno;
+    private javax.swing.JLabel PlanetaRoto;
     private javax.swing.JLabel Tierra;
     private javax.swing.JLabel Universo;
     private javax.swing.JLabel Venus;
     private javax.swing.JButton Volver;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel5;
     // End of variables declaration//GEN-END:variables
 }
